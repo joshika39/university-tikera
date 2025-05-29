2150fd0bb0036f76a9ca0511b0d7ef8fdaee6b89
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-05-29 20:42:11 +0200

    feat: added theme changing dropdown

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -4,6 +4,7 @@ import {buttonVariants} from "@/components/ui/button";
 import Logo from "@/assets/ticket.svg?react";
 import {Toaster} from "@/components/ui/sonner";
 import {ThemeColorProvider} from "@/layouts/theme-color-provider";
+import {ThemeChanger} from "@/components/theme-changer";
 
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
@@ -13,11 +14,14 @@ export const RootLayout = () => {
       <ThemeColorProvider/>
       <div
         className="flex flex-col gap-2 bg-gradient-to-br from-background via-background via-60% to-primary/20 md:to-primary/40 min-h-screen h-full w-fit">
-        <div className="md:absolute md:top-4 md:left-8 p-4 md:p-0 self-center flex flex-row gap-2 items-center">
-          <Logo className="h-8 text-primary"/>
-          <h1 className="text-3xl font-bold text-primary">
-            Tikera
-          </h1>
+        <div className="md:absolute md:top-4 p-4 md:px-4 md:py-0 self-center flex flex-row gap-4 items-center justify-between w-auto">
+          <div className="flex flex-row gap-2 items-center">
+            <Logo className="h-8 text-primary"/>
+            <h1 className="text-3xl font-bold text-primary">
+              Tikera
+            </h1>
+          </div>
+          <ThemeChanger />
         </div>
         <div className="flex flex-wrap flex-row gap-2 items-center self-center mt-4">
           {days.map((day, index) => (
